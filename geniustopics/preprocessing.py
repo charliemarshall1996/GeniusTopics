f072fd23a586966eab17528667c7d4df95bf6606
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-01-20 13:13:00 +0000

    Refractored remove_new_line()

--- geniustopics/preprocessing.py
+++ geniustopics/preprocessing.py
@@ -1,7 +1,18 @@
 
-def remove_new_line(string: str):
-    try:
-        string = string.replace("\n", "")
-        return string.replace("\r", "")
-    except (TypeError, AttributeError):
+def remove_new_line(input_string: str):
+    """
+    Remove new line and carriage return characters from the
+    input string.
+
+    Args:
+    - `input_string` (`str`): The input string from which new
+    line and carriage return 
+        characters need to be removed.
+
+    Returns:
+    - `str`: The input string with new line and carriage return
+    characters removed.
+    """
+    if not isinstance(input_string, str):
         return ""
+    return input_string.replace("\n", "").replace("\r", "")
